99fef1da7d61407db97f4e650f0ed0a061db51df
Fredric Dorothy <fredric.dorothy@vipaar.com>
2019-10-11 11:17:12 -0500

    added build

---
+++ build/precache-manifest.3d6b55f6ea4ec3db46e6084bc6f04b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26e501d429ef594f4fb9ac23ca3c96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bafa0e728815e3f4e260",
+    "url": "/static/css/main.5c1b7067.chunk.css"
+  },
+  {
+    "revision": "1718ca18a0721dda3568",
+    "url": "/static/js/2.8801eca5.chunk.js"
+  },
+  {
+    "revision": "bafa0e728815e3f4e260",
+    "url": "/static/js/main.2043468f.chunk.js"
+  },
+  {
+    "revision": "2be77165aa68a499189a",
+    "url": "/static/js/runtime-main.4f52a914.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
